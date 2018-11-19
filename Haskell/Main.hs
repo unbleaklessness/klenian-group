473431cc4400269da256ffe86d617f6413c3f94a
oliegjio <oliegjio@gmail.com>
2018-11-19 22:39:44 +0300

    Refactored Haskell code

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -11,16 +11,14 @@ fixedPoint :: Eq a => (a -> a) -> a -> a
 fixedPoint f x = converge (==) $ iterate f x
 
 combinations :: [a] -> [b] -> [[(a, b)]]
-combinations l1 l2 = map (\a -> map (\b -> (a, b)) l2) l1
+combinations l1 l2 = (\a -> (\b -> (a, b)) <$> l2) <$> l1
 
 outer :: ((a, b) -> (c, d)) -> [a] -> [b] -> [[(c, d)]]
-outer f l1 l2 = map (\a -> map (\b -> f b) a) $ combinations l1 l2
+outer f l1 l2 = (\a -> (\b -> f b) <$> a) <$> combinations l1 l2
 
 -- generator l = 
 
 main :: IO ()
 main = do
     print $ fixedPoint (\x -> (x + 2 / x) / 2) 1
-    print $ map (\a -> take 2 a) $ permutations [1, 2, 3]
     print $ outer (\(a, b) -> (succ a, pred b)) ['a', 'b', 'c'] [1, 2, 3]
-    print "test" 
