fdf4db385a0141d1e1762f4229ee678ac7f56835
oliegjio <oliegjio@gmail.com>
2018-11-21 23:18:39 +0300

    Added Haskell group generator

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Data.List (union, permutations)
+import Data.List (transpose, nub)
+import Data.Fixed (mod')
 
 converge :: (a -> a -> Bool) -> [a] -> a
 converge p (x:ys@(y:_))
@@ -11,14 +12,29 @@ fixedPoint :: Eq a => (a -> a) -> a -> a
 fixedPoint f x = converge (==) $ iterate f x
 
 combinations :: [a] -> [b] -> [[(a, b)]]
-combinations l1 l2 = (\a -> (\b -> (a, b)) <$> l2) <$> l1
+combinations l1 l2 = (\ a -> (\ b -> (a, b)) <$> l2) <$> l1
 
-outer :: ((a, b) -> (c, d)) -> [a] -> [b] -> [[(c, d)]]
-outer f l1 l2 = (\a -> (\b -> f b) <$> a) <$> combinations l1 l2
+outer :: (a -> b -> c) -> [a] -> [b] -> [[c]]
+outer f l1 l2 = (\ a -> (\ b -> uncurry f b) <$> a) <$> combinations l1 l2
 
--- generator l = 
+matMult :: Num a => [[a]] -> [[a]] -> [[a]]
+matMult a b = [[sum $ zipWith (*) ar bc | bc <- (transpose b)] | ar <- a]
+
+matMultMod :: Real a => a -> [[a]] -> [[a]] -> [[a]]
+matMultMod modulus a b = [[mod' (sum $ zipWith (*) ar bc) modulus | bc <- (transpose b)] | ar <- a]
+
+generator :: Real a => a -> [[[a]]] -> [[[a]]]
+generator modulus = fixedPoint (\ a -> nub $ concat $ outer (\ x y -> matMultMod modulus x y) a a)
 
 main :: IO ()
 main = do
-    print $ fixedPoint (\x -> (x + 2 / x) / 2) 1
-    print $ outer (\(a, b) -> (succ a, pred b)) ['a', 'b', 'c'] [1, 2, 3]
+    print $ fixedPoint (\ x -> (x + 2 / x) / 2) 1
+    print $ concat $ outer (\ a b -> (succ a, pred b)) ['a', 'b', 'c'] [1, 2, 3]
+    
+    let initials = [[[0, 3], [2, 4]], [[0, 1], [6, 0]],[[1, 1], [0, 1]], [[3, 0], [0, 5]]]
+    let group = generator 7 initials
+    
+    print $ group
+    print $ length group
+
+    return ()
