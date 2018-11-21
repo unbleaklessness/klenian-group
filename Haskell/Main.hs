60360cacd9ee824c345b4ef8ddbfd229d96d71b9
oliegjio <oliegjio@gmail.com>
2018-11-21 23:21:04 +0300

    Changed group generator name

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -23,8 +23,8 @@ matMult a b = [[sum $ zipWith (*) ar bc | bc <- (transpose b)] | ar <- a]
 matMultMod :: Real a => a -> [[a]] -> [[a]] -> [[a]]
 matMultMod modulus a b = [[mod' (sum $ zipWith (*) ar bc) modulus | bc <- (transpose b)] | ar <- a]
 
-generator :: Real a => a -> [[[a]]] -> [[[a]]]
-generator modulus = fixedPoint (\ a -> nub $ concat $ outer (\ x y -> matMultMod modulus x y) a a)
+generateGroup :: Real a => a -> [[[a]]] -> [[[a]]]
+generateGroup modulus = fixedPoint (\ a -> nub $ concat $ outer (\ x y -> matMultMod modulus x y) a a)
 
 main :: IO ()
 main = do
@@ -32,7 +32,7 @@ main = do
     print $ concat $ outer (\ a b -> (succ a, pred b)) ['a', 'b', 'c'] [1, 2, 3]
     
     let initials = [[[0, 3], [2, 4]], [[0, 1], [6, 0]],[[1, 1], [0, 1]], [[3, 0], [0, 5]]]
-    let group = generator 7 initials
+    let group = generateGroup 7 initials
     
     print $ group
     print $ length group
