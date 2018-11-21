9fe2ff86a763a4ccbf0ee10d624062c1d5029325
oliegjio <oliegjio@gmail.com>
2018-11-21 23:31:55 +0300

    Refactored matrix multiplication with modulus

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -21,17 +21,14 @@ matMult :: Num a => [[a]] -> [[a]] -> [[a]]
 matMult a b = [[sum $ zipWith (*) ar bc | bc <- (transpose b)] | ar <- a]
 
 matMultMod :: Real a => a -> [[a]] -> [[a]] -> [[a]]
-matMultMod modulus a b = [[mod' (sum $ zipWith (*) ar bc) modulus | bc <- (transpose b)] | ar <- a]
+matMultMod modulus a b = (map . map) (flip mod' modulus) (matMult a b)
 
 generateGroup :: Real a => a -> [[[a]]] -> [[[a]]]
 generateGroup modulus = fixedPoint (\ a -> nub $ concat $ outer (\ x y -> matMultMod modulus x y) a a)
 
 main :: IO ()
 main = do
-    print $ fixedPoint (\ x -> (x + 2 / x) / 2) 1
-    print $ concat $ outer (\ a b -> (succ a, pred b)) ['a', 'b', 'c'] [1, 2, 3]
-    
-    let initials = [[[0, 3], [2, 4]], [[0, 1], [6, 0]],[[1, 1], [0, 1]], [[3, 0], [0, 5]]]
+    let initials = [[[0, 3], [2, 4]], [[0, 1], [6, 0]], [[1, 1], [0, 1]], [[3, 0], [0, 5]]]
     let group = generateGroup 7 initials
     
     print $ group
