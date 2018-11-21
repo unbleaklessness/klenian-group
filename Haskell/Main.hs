4d6e6aa8a5733f186740d914067927aaeebf21b5
oliegjio <oliegjio@gmail.com>
2018-11-22 02:46:01 +0300

    Done Cayley table generation, refactoring

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -28,13 +28,13 @@ generateGroup :: Real a => a -> [[[a]]] -> [[[a]]]
 generateGroup modulus = fixedPoint (\ a -> nub $ concat $ outer (matMultMod modulus) a a)
 
 cayleyTable :: Real a => a -> [[[a]]] -> [[Int]]
-cayleyTable modulus group = [[fromMaybe (-1) $ products !! i !! j `elemIndex` group | j <- [0 .. len]] | i <- [0 .. len]]
+cayleyTable modulus group = [[fromMaybe (-1) $ products !! i !! j `elemIndex` group | j <- [0 .. l]] | i <- [0 .. l]]
     where products = outer (matMultMod modulus) group group
-          len = length group - 1
+          l = length group - 1
 
 identity :: Num a => Int -> [[a]]
-identity dimensions = [[fromIntegral $ fromEnum $ i == j | j <- [0 .. len]] | i <- [0 .. len]]
-    where len = dimensions - 1
+identity dimensions = [[fromIntegral $ fromEnum $ i == j | j <- [0 .. l]] | i <- [0 .. l]]
+    where l = dimensions - 1
 
 secondIdentity :: Num a => Int -> a -> [[a]]
 secondIdentity dimensions modulus = (map . map) (* (modulus - 1)) (identity dimensions)
@@ -46,21 +46,23 @@ removeItem x (y:ys) | x == y = removeItem x ys
 
 groupNub :: Real a => a -> [[[a]]] -> [[[a]]]
 groupNub modulus group = foldl check group group
-    where left = secondIdentity (length (group !! 0)) modulus
-          check a b = if element `elem` a then removeItem b a else a
-              where element = matMultMod modulus left b
+    where i = secondIdentity (length (group !! 0)) modulus
+          check a b = if element b `elem` a then removeItem b a else a
+          element = matMultMod modulus i
 
 main :: IO ()
 main = do
     let initials = [[[0, 3], [2, 4]], [[0, 1], [6, 0]], [[1, 1], [0, 1]], [[3, 0], [0, 5]]]
     let modulus = 7
+    
     let group = generateGroup modulus initials
     let groupUnique = groupNub modulus group
-    let cayley = cayleyTable modulus group
+    let cayley = cayleyTable modulus groupUnique
     
     print $ group
     print $ length group
     print $ groupUnique
     print $ length groupUnique
+    print $ length cayleyUnique
 
     return ()
