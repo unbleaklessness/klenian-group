4c43288af044c1da18411216583462bd0929883e
oliegjio <oliegjio@gmail.com>
2018-11-23 22:49:56 +0300

    Fixed Cayley table, refactoring...

--- Haskell/Main.hs
+++ Haskell/Main.hs
@@ -16,7 +16,7 @@ combinations :: [a] -> [b] -> [[(a, b)]]
 combinations l1 l2 = [[(a, b) | a <- l1] | b <- l2]
 
 outer :: (a -> b -> c) -> [a] -> [b] -> [[c]]
-outer f l1 l2 = (fmap . fmap) (uncurry f) (combinations l1 l2)
+outer f l1 l2 = (map . map) (uncurry f) (combinations l1 l2)
 
 matMult :: Num a => [[a]] -> [[a]] -> [[a]]
 matMult a b = [[sum $ zipWith (*) ar bc | bc <- (transpose b)] | ar <- a]
@@ -34,42 +34,55 @@ cayleyTable :: Real a => a -> [[[a]]] -> [[Int]]
 cayleyTable modulus group = (map . map) (fromMaybe (-1) . flip elemIndex group) products
     where products = outer (matMultMod modulus) group group
 
-identity :: Num a => Int -> [[a]]
-identity dimensions = [[fromIntegral $ fromEnum $ i == j | j <- [0 .. l]] | i <- [0 .. l]]
+identity1 :: Num a => Int -> [[a]]
+identity1 dimensions = [[fromIntegral $ fromEnum $ i == j | j <- [0 .. l]] | i <- [0 .. l]]
     where l = dimensions - 1
 
 identity2 :: Num a => Int -> a -> [[a]]
-identity2 dimensions modulus = (map . map) (* (modulus - 1)) (identity dimensions)
+identity2 dimensions modulus = (map . map) (* (modulus - 1)) (identity1 dimensions)
+
+identity1Index :: Real a => [[[a]]] -> Int
+identity1Index group = fromMaybe (-1) $ identity1 (length (group !! 0)) `elemIndex` group
+
+identity2Index :: Real a => a -> [[[a]]] -> Int
+identity2Index modulus group = fromMaybe (-1) $ identity2 (length (group !! 0)) modulus `elemIndex` group
 
 removeItem :: Eq a => a -> [a] -> [a]
 removeItem _ [] = []
 removeItem x (y:ys) | x == y = removeItem x ys
                     | otherwise = y : removeItem x ys
 
-groupNub :: Real a => a -> [[[a]]] -> [[[a]]]
-groupNub modulus group = foldl check group group
-    where i = identity2 (length (group !! 0)) modulus
-          check a b = if e b `elem` a then removeItem b a else a
-          e = matMultMod modulus i
+removeIndex :: Int -> [a] -> [a]
+removeIndex _ [] = []
+removeIndex i (a:as) | i == 0 = as
+                     | otherwise = a : removeIndex (i - 1) as
 
-cayleyProducts :: Int -> [(Int, Int)]
-cayleyProducts n = [(i, j) | i <- [0 .. n], j <- [0 .. n]]
+removeIndexes :: [Int] -> [a] -> [a]
+removeIndexes indexes list = foldl (flip removeIndex) list indexes
 
-filterProducts :: ((Int, Int) -> Bool) -> [(Int, Int)] -> [(Int, Int)]
-filterProducts f products = nub $ so <$> filter f products
+removeIndexes2 :: [Int] -> [[a]] -> [[a]]
+removeIndexes2 indexes table = removeIndexes indexes $ map (removeIndexes indexes) table
+
+equivalentIndexes :: [[Int]] -> Int -> [Int]
+equivalentIndexes cayley idIndex = foldl check [] [0 .. length cayley - 1]
+    where check a b = if cayley !! idIndex !! b `elem` a then a else b : a
+
+pairs :: Int -> [(Int, Int)]
+pairs n = [(i, j) | i <- [0 .. n], j <- [0 .. n]]
+
+filterPairs :: ((Int, Int) -> Bool) -> [(Int, Int)] -> [(Int, Int)]
+filterPairs f pairs = nub $ so <$> filter f pairs
     where so (a, b) = if a > b then (b, a) else (a, b)
 
 getCommutatives :: [[Int]] -> [(Int, Int)] -> [(Int, Int)]
-getCommutatives cayley products = filterProducts check products
+getCommutatives cayley pairs = filterPairs check pairs
     where check (i, j) = cayley !! i !! j == cayley !! j !! i
 
 getInverses :: [[Int]] -> [(Int, Int)] -> Int -> [(Int, Int)]
-getInverses cayley products identityIndex = filterProducts check products
-    where check (i, j) = cayley !! i !! j == identityIndex
+getInverses cayley pairs idIndex = filterPairs check pairs
+    where check (i, j) = cayley !! i !! j == idIndex
 
-identity2Index :: Real a => [[[a]]] -> a -> Int
-identity2Index group modulus = fromMaybe (-1) $ identity2 (length (group !! 0)) modulus `elemIndex` group
-
+-- Multiplication of matrix M1 by M2 is: group !! (cayleyUnique !! m2index !! m1index).
 main :: IO ()
 main = do
     let initials = [[[0, 3], [2, 4]], [[0, 1], [6, 0]], [[1, 1], [0, 1]], [[3, 0], [0, 5]]]
@@ -84,16 +97,40 @@ main = do
     -- let initials = [[[2, 0], [0, 7]], [[0, 5], [5, 3]], [[1, 1], [0, 1]], [[0, 1], [12, 0]], [[2, 2], [3, 10]]]
     -- let modulus = 12
   
-    let group = groupNub modulus $ generateGroup modulus initials
+    let group = generateGroup modulus initials
     let cayley = cayleyTable modulus group
-    let iden2 = identity2Index group modulus
-    let products = cayleyProducts $ length cayley - 1
-    let commutative = getCommutatives cayley products
-    let inverses = getInverses cayley products iden2
+    let iden1 = identity1Index group
+    let iden2 = identity2Index modulus group
+    let eqIndexes = equivalentIndexes cayley iden2
+    let groupUnique = removeIndexes eqIndexes group
+    let cayleyUnique = removeIndexes2 eqIndexes cayley
+    let multPairs = pairs $ length groupUnique - 1
+    -- let multPairs = pairs $ length group - 1
+    let commutatives = getCommutatives cayleyUnique multPairs
+    -- let commutatives = getCommutatives cayley multPairs
+    let inverses = getInverses cayleyUnique multPairs iden1
+    -- let inverses = getInverses cayley multPairs iden1
 
     print $ group
     print $ length group
-    print $ length $ commutative
-    print $ length $ inverses
-    
+    print $ groupUnique
+    print $ length groupUnique
+    -- print $ cayley
+    -- print $ length cayley
+    -- print $ cayleyUnique
+    -- print $ length cayleyUnique
+    print $ inverses
+    print $ length inverses
+    print $ commutatives
+    print $ length commutatives
+
+    print "=== TEST MULTIPLICATION ==="
+
+    let i1 = 29
+    let i2 = 76
+    print $ matMultMod modulus (groupUnique !! i1) (groupUnique !! i2)
+    print $ group !! (cayleyUnique !! i2 !! i1)
+
     return ()
+
+
